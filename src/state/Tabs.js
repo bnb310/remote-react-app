529b3fb641def82341eaac3e36d0471ee195cb03
bnb310 <brittany.n.baird@gmail.com>
2019-08-21 11:14:10 -0600

    Completed buttons working I think

--- src/state/Tabs.js
+++ src/state/Tabs.js
@@ -2,20 +2,32 @@ import React from "react";
 
 class Tabs extends React.Component {
   static defaultProps = {tabs: []};
-  render() {
-    const buttons = this.props.tabs.map((tab, index) => (
-      <button key={index}>
+  state = {
+    currentTabIndex: 0
+  }
+  handleButtonClick(index) {
+    console.log('button clicked!', { index })
+  }
+  renderButtons() {
+    return this.props.tabs.map((tab, index) => (
+      <button key={index} onClick={() => this.handleButtonClick(index)}>
         {tab.name}
       </button>
     ))
-    const currentTab = this.props.tabs[0]
+  }
+  renderContent() {
+    const currentTab = this.props.tabs[this.state.currentTabIndex]
+    return (
+      <div className='content'>
+        {currentTab.content}
+      </div>
+    )
+  }
+  render() {
     return (
       <div>
-        {buttons}
-        {this.props.tabs.length && (
-          <div className='content'>
-          {currentTab.content}
-        </div>)}
+        {this.renderButtons()}
+        {this.props.tabs.length && this.renderContent()}
       </div>
     )
   }
