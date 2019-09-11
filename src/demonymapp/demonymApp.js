1716490a8be29b831029cd9f8dc5c96227b2612d
bnb310 <brittany.n.baird@gmail.com>
2019-09-11 16:01:01 -0600

    finished demonym App

--- src/demonymapp/demonymApp.js
+++ src/demonymapp/demonymApp.js
@@ -20,13 +20,27 @@ class DemonymApp extends Component {
   }
 
   componentDidMount() {
-    fetch('https://country.register.gov.uk/records.json?page-size=5000')
+    fetch('https://country.register.gov.uk/recordsWRONG.json?page-size=5000')
+      .then(response => {
+        //check if response is ok
+        console.log('About to check for errors');
+        if(!response.ok) {
+          console.log('An error did occur, let\'s throw an error.');
+          throw new Error('Something went wrong'); //throw an error
+        }
+        return response; //ok, so just continue
+      })
       .then(response => response.json())
       .then(data => {
         const countries = Object.keys(data)
               .map(key => data[key].item[0]);
         this.setState({
           countries
+        });
+      })
+      .catch(err => {
+        this.setState({
+        error: err.message
         });
       });
   }
@@ -35,6 +49,10 @@ class DemonymApp extends Component {
     const demon = this.state.selected
           ? <Demonym name={this.state.selected['citizen-names']} country={this.state.selected.name}/>
           : <div className="demonym_app__placeholder">Select a country above</div>;
+
+    const error = this.state.error
+    ? <div className="demonym_app__error">{this.state.error}</div>
+    : "";
 
     return (
       <div className="demonym_app">
